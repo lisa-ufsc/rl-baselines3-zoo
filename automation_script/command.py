573162868c09c78b2aeea75faeb3f15f9d52df4c
Augusto <augusto.h.v.guerner@gmail.com>
2022-12-21 15:35:01 -0300

    python3 --algo tqc --env HopperBulletEnv-v0 -f experiments/exploration/seed_3372438727/n_steps_64 --reward-log experiments/exploration/rewards/seed_3372438727/n_steps_64/tqc/HopperBulletEnv-v0 -n 1000000 --load-best --no-render

--- automation_script/command.py
+++ automation_script/command.py
@@ -2,7 +2,23 @@
 
 class Command:
 
-    def __init__(self, train_command, enjoy_command) -> None:
-        self.__train_command = ""
-        self.__enjoy_command = ""
+    def __init__(self, train_command: list = [], enjoy_command: list = []) -> None:
+        self.__train_command = train_command
+        self.__enjoy_command = enjoy_command
+
+    @property
+    def train_command(self) -> list:
+        return self.__train_command
+
+    @train_command.setter
+    def train_command(self, new_train_command: list) -> None:
+        self.__train_command = new_train_command
+
+    @property
+    def enjoy_command(self) -> list:
+        return self.__enjoy_command
+
+    @enjoy_command.setter
+    def enjoy_command(self, new_enjoy_command) -> None:
+        self.__enjoy_command = new_enjoy_command
 
